5089e8dc3d270b7335108a1f2c3329d8b5e7d412
Kenneth Lee <kennyxlee@gmail.com>
2015-07-11 12:21:22 -0700

    fizzbuzz (js)

---
+++ fizzbuzz.js
@@ -0,0 +1,17 @@
+// "Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”."
+
+var fizzBuzz = function(upperBound) {
+  for (var num = 1; num <= upperBound; num++) {
+    if (num % 3 === 0 && num % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (num % 3 === 0) {
+        console.log("Fizz");
+    } else if (num % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(num);
+    }
+  }
+}
+
+fizzBuzz(100);
