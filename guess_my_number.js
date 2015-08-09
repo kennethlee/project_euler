f01ec8090a3a7787cf22ac74f72afe14ca3a5e11
Kenneth Lee <kennyxlee@gmail.com>
2015-08-09 16:21:53 -0700

    guess my number (js)

---
+++ guess_my_number.js
@@ -0,0 +1,12 @@
+// Guess My Number
+// https://www.daniweb.com/software-development/python/threads/131973/5-crucial-projects-for-beginners
+
+// The computer randomly generates a number. The user inputs a number, and the
+// computer will tell you if you are too high, or too low. Then you will get to
+// keep guessing until you guess the number.
+
+var guessMyNumber = function(n) {
+  var correctNumber = Math.floor((Math.random() * n) + 1);
+}
+
+guessMyNumber(100);
